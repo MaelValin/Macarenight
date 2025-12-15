9225a07f1ad636282410042fcab356e74d0e2937
MaelValin <maelmy19@gmail.com>
2025-12-15 15:20:51 +0100

    coorection formatage

--- docs/ServiceWorker.js
+++ docs/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "WakerIndustries-Macarenight-1.0.0";
 const contentToCache = [
     "Build/docs.loader.js",
-    "Build/docs.framework.js.gz",
-    "Build/docs.data.gz",
-    "Build/docs.wasm.gz",
+    "Build/docs.framework.js",
+    "Build/docs.data",
+    "Build/docs.wasm",
     "TemplateData/style.css"
 
 ];
